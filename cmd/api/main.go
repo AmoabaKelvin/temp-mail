795e305883bdf5b0c1c2231dd3c430cf9c63b3d0
AmoabaKelvin <kel.amoaba@gmail.com>
2025-05-03 19:26:31 +0000

    refactor(api): remove dotenv loading from main function

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,15 +11,9 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	config, err := config.Load()
 	if err != nil {
 		log.Fatal("Error loading config")
