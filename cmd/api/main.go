6851d86db53b66bff3e113f5f7ced21c6e95cde8
AmoabaKelvin <kel.amoaba@gmail.com>
2025-05-17 10:15:57 +0000

    refactor(api): restructure main application logic and configuration handling
    - Simplified the main function by removing the handlers and config packages.
    - Introduced a new application struct to encapsulate configuration and repository.
    - Updated database connection handling to utilize environment variables directly.
    - Enhanced server startup logic with improved error handling and routing setup.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"log"
-	"net/http"
+	"os"
+	"strings"
 
 	"github.com/AmoabaKelvin/temp-mail/internal/database"
-	"github.com/AmoabaKelvin/temp-mail/internal/handlers"
 	"github.com/AmoabaKelvin/temp-mail/internal/repository"
-	"github.com/AmoabaKelvin/temp-mail/pkg/config"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
+
 	"github.com/joho/godotenv"
 )
 
@@ -20,36 +17,32 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	config, err := config.Load()
-	if err != nil {
-		log.Fatal("Error loading config")
+	config := &config{
+		addr: os.Getenv("ADDR"),
+		db: &dbConfig{
+			addr: os.Getenv("DATABASE_URL"),
+		},
+		tempMail: &tempMailConfig{
+			domains:           strings.Split(os.Getenv("TEMPMAIL_DOMAINS"), ","),
+			expireAfter:       os.Getenv("EXPIRE_AFTER"),
+			expirationEnabled: os.Getenv("EXPIRATION_ENABLED"),
+		},
 	}
 
-	db, err := database.New(config.DatabaseURL)
+	db, err := database.New(config.db.addr)
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	repository := repository.New(db)
-	handler := handlers.New(repository, config)
+	app := &application{
+		config: config,
+		store:  repository,
+	}
 
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	routes := app.mount()
 
-	r.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedOrigins:   []string{"http://localhost:3000"}, // Adjust to your frontend URL
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}))
-
-	r.Post("/v1/addresses", handler.GenerateAddress)
-	r.Get("/v1/messages", handler.GetMessages)
-	r.Delete("/v1/messages/{id}", handler.DeleteMessage)
-	r.Put("/v1/messages/{id}/read", handler.UpdateMessageReadAt)
-	log.Println("Starting server on :8080")
-	http.ListenAndServe(":8080", r)
+	if err := app.run(routes); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
