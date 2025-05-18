62c3e7fb2ba0bd040808755bbc34840da56bd02b
AmoabaKelvin <kel.amoaba@gmail.com>
2025-05-18 08:55:06 +0000

    feat(api): update CORS configuration to allow new origin
    - Added "https://www.is-temp.com" to the list of allowed origins in the CORS middleware for the API.
    - This change enhances the API's accessibility by permitting requests from the new domain.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -35,7 +35,7 @@ func (app *application) mount() http.Handler {
 	r.Use(middleware.Logger)
 
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:3000/*"},
+		AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:3000/*", "https://www.is-temp.com"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
